c358e0af784ee7645529e402ec661a6ca16a636e
gvinay2907 <vinayyyyy007@gmail.com>
2026-03-10 18:55:19 +0530

    fix render port

--- app.py
+++ app.py
@@ -959,10 +959,7 @@ if __name__ == "__main__":
         db.create_all()
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
